40f44b29683dc65547d184de3006d6122ee13776
Marek Baczynski <imbaczek@gmail.com>
2009-03-30 00:33:14 +0200

    add checking for existing BUILD_EXPANSION goals

--- TopLevelAI.cpp
+++ TopLevelAI.cpp
@@ -1,3 +1,4 @@
+#include <strstream>
 #include <boost/foreach.hpp>
 
 #include "ExternalAI/IGlobalAICallback.h"
@@ -40,6 +41,11 @@ bool TopLevelAI::ProcessGoal(Goal* g)
 		default:
 			ailog->info() << "unknown goal type: " << g->type << " params "
 				<< g->params.size() << std::endl;
+			std::stringstream ss;
+			BOOST_FOREACH(Goal::param_variant p, g->params) {
+				ss << p << ", ";
+			}
+			ailog->info() << "params: " << ss.str() << endl;
 	}
 	return false;
 }
@@ -49,9 +55,33 @@ void TopLevelAI::FindGoals()
 	ailog->info() << "FindGoal()" << std::endl;
 	BOOST_FOREACH(float3 geo, ai->geovents) {
 		// TODO priority should be a function of distance and risk
-		Goal *g = Goal::GetGoal(Goal::CreateGoal(1, BUILD_EXPANSION));
+		int priority = ai->cb->GetCurrentFrame();
 		// check if there already is a goal with this position
 		// or if somebody built an expansion there
+		BOOST_FOREACH(int gid, goals) {
+			Goal* goal = Goal::GetGoal(gid);
+			if (goal->type != BUILD_EXPANSION)
+				continue;
+			if (goal->priority == priority)
+				continue;
+			if (goal->params.empty()) {
+				ailog->error() << "TopLevel BUILD_EXPANSION without param, removing" << endl;
+				Goal::RemoveGoal(goal);
+				continue;
+			}
+			float3 *param = boost::get<float3>(&goal->params[0]);
+			if (!param) {
+				ailog->error() << "TopLevel BUILD_EXPANSION with param 0 not float3 (" << goal->params[0] << "), removing" << endl;
+				Goal::RemoveGoal(goal);
+				continue;
+			}
+			if (param->SqDistance2D(geo) < 1) {
+				ailog->info() << "aborting old BUILD_EXPANSION goal at " << param << endl;
+				Goal::RemoveGoal(goal);
+			}
+		}
+		// add the goal
+		Goal *g = Goal::GetGoal(Goal::CreateGoal(priority, BUILD_EXPANSION));
 		g->params.push_back(geo);
 		AddGoal(g);
 	}
