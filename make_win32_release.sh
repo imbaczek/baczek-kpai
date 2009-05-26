f435ead7942ec6d4abd1aaaa729d9a55bf708acc
Marek Baczynski <imbaczek@gmail.com>
2009-05-26 12:52:05 +0200

    significant improvement to release script

--- make_win32_release.sh
+++ make_win32_release.sh
@@ -2,11 +2,28 @@
 
 AINAME="Baczek's KP AI"
 AISHORTNAME=BaczekKPAI
-AIDLL=output/default/SkirmishAI.dll
 TEMPDIR=output/temp
 SKIRMISHAIDIR=AI/Skirmish
-DEPSDIR=dependencies_gcc
+DEPSDIRS="deps/common" 
+PYLIB="deps/python/library.zip"
 ZIP="7za a -tzip"
+
+case $1 in
+	gcc)
+		AIDLL=output/default/SkirmishAI.dll
+		DEPSDIRS="$DEPSDIRS deps/gcc"
+		KIND=gcc
+		;;
+	msvc)
+		AIDLL=SkirmishAI.dll
+		DEPSDIRS="$DEPSDIRS deps/msvc"
+		KIND=msvc
+		;;
+	*)
+		echo "unknown compiler \"$1\" or missing option, aborting"
+		exit 1
+		;;
+esac
 
 if [ ! -f "$AIDLL" ]; then
 	echo "$AIDLL doesn't exist, aborting"
@@ -23,8 +40,9 @@ mkdir -p "$TEMPDIR" || exit 1
 # get version
 AIVERSION=$(cat data/AIInfo.lua | grep -- "-- AI version"  \
 		| awk '{ print $3 }' | sed -e "s/[\\\"',]//g" )
+GITVERSION=$(git rev-parse HEAD | sed 's/\(.......\).*/\1/')
 
-echo $AINAME $AIVERSION
+echo $AINAME $AIVERSION $GITVERSION
 
 # create directory structure
 
@@ -37,15 +55,18 @@ mkdir -p "$DISTDIR/py" || exit 1
 cp -p "$AIDLL" "$DISTDIR"
 cp -p data/*.lua "$DISTDIR"
 cp -p data/py/*.py "$DISTDIR/py"
+cp -p "$PYLIB" "$DISTDIR/py"
 
 # docs
 cp -p README_BaczekKPAI.txt "$TEMPDIR"
 
 # dll dependencies
-cp -p "$DEPSDIR"/* "$TEMPDIR"
+for dep in $DEPSDIRS; do
+	cp -p "$dep"/* "$TEMPDIR"
+done
 
 DIR=`pwd`
-ARCHIVENAME="$DIR/$AISHORTNAME-$AIVERSION.zip"
+ARCHIVENAME="$DIR/$AISHORTNAME-$AIVERSION-$KIND-$GITVERSION.zip"
 
 (cd "$TEMPDIR" && $ZIP "$ARCHIVENAME" *)
 
